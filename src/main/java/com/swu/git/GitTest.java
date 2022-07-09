d53a4cb777f24f494f515194060c3563432ecda9
lfg <644726133@qq.com>
2022-07-09 15:33:31 +0800

    hot_fix commit1

--- src/main/java/com/swu/git/GitTest.java
+++ src/main/java/com/swu/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("IDEA git");
         System.out.println("IDEA git");
+        System.out.println("IDEA git1");
     }
 }
