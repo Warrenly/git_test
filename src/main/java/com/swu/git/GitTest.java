8956e1abe93e7f9c050d469baa5e3913e383092a
lfg <644726133@qq.com>
2022-07-09 15:55:05 +0800

    github commit

--- src/main/java/com/swu/git/GitTest.java
+++ src/main/java/com/swu/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
         System.out.println("IDEA git1");
         System.out.println("master git");
         System.out.println("hot_fix git");
+        System.out.println("github push");
     }
 }
